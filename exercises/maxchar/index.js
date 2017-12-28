5b4de089f7965d400d6afd3842acd9566ea0de99
William <he.william@yahoo.com>
2017-12-28 15:40:38 -0800

    finished maxhcar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,28 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let newObj = {};
+    let highestVal = 0;
+    let maxletter = "";
+    str.split("").forEach((ele)=>{
+        if(newObj[ele] === undefined) {
+            newObj[ele] = 1
+        } else {
+            newObj[ele] ++
+        }
+    })
+    console.log(newObj)
+
+    for(let key in newObj) {
+        if(highestVal < newObj[key]) {
+            highestVal = newObj[key]
+            maxletter = key;
+        }
+    }
+    return maxletter
+}
+
+console.log(maxChar('asddddf'))
 
 module.exports = maxChar;
